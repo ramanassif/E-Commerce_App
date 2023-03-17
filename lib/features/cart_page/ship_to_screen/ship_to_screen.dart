38e605db871d38dbc9b0b5e109b277264a3e58aa
RamaNassif <rama.nassif.4@gmail.com>
2023-03-17 15:09:42 +0300

    Delete an address from ShipToScreen when press on delete icon

--- lib/features/cart_page/ship_to_screen/ship_to_screen.dart
+++ lib/features/cart_page/ship_to_screen/ship_to_screen.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/basics_widgets/custom_button.dart';
-import 'package:ecommerce_app/features/cart_page/ship_to_screen/widgets/ship_to_details.dart';
 import 'package:ecommerce_app/features/cart_page/ship_to_screen/widgets/ship_to_screen_header.dart';
 import 'package:flutter/material.dart';
 
@@ -47,10 +46,123 @@ class _ShipToScreenState extends State<ShipToScreen> {
                         currentIndex = index;
                       });
                     },
-                    child: ShipToDetails(
-                      firstName: shipName[index],
-                      isSelected: currentIndex == index ? true : false,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16.0,
+                        right: 16.0,
+                        top: 16.0,
+                      ),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 240,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: currentIndex == index
+                                  ? kPrimaryColor
+                                  : kLightColor),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(24.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                shipName[index],
+                                style: const TextStyle(
+                                  color: kSecondaryColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              const Text(
+                                '3711 Spring Hill Rd undefined Tallahassee, Nevada 52874 United States',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              const Text(
+                                '+99 1234567890',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, 'addAddressScreen',
+                                          arguments: {
+                                            'first_name': shipName[index],
+                                          });
+                                    },
+                                    child: Container(
+                                      width: 77,
+                                      height: 57,
+                                      decoration: BoxDecoration(
+                                        color: kPrimaryColor,
+                                        borderRadius: BorderRadius.circular(5),
+                                        boxShadow: const [
+                                          BoxShadow(
+                                            offset: Offset(0, 4),
+                                            blurRadius: 5.0,
+                                            color: kPrimaryColor,
+                                          ),
+                                        ],
+                                      ),
+                                      child: const Center(
+                                        child: Text(
+                                          'Edit',
+                                          style: TextStyle(
+                                            color: kWhiteColor,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 30,
+                                  ),
+                                  GestureDetector(
+                                    onTap: (){
+                                      setState(() {
+                                        shipName.removeAt(index);
+                                      });
+                                    },
+                                    child: const Icon(
+                                      Icons.delete_outline,
+                                      color: Colors.grey,
+                                      size: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
+
+                    // ShipToDetails(
+                    //   firstName: shipName[index],
+                    //   isSelected: currentIndex == index ? true : false,
+                    // ),
                   ),
                 ),
               ),
